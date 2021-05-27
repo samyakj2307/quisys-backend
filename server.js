0a1163068eeeaa854a2b423a9924e679e46e903f
Samyak Jain <jainsamyak2300@gmail.com>
2021-05-27 23:55:29 +0530

    added Student Get Post Methods and faculty checking endPoints

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 
 const app = express();
+process.setMaxListeners(0)  //due to memory leak yeh add karna pada
 
 app.use(bodyParser.json());
 // app.use(express.static("public"));
@@ -17,6 +18,7 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+////////////////////////// Faculty Schemas START///////////////////////////////////////////////
 const optionSchema = {
   value: { type: String, required: true },
 };
@@ -30,7 +32,7 @@ const questionSchema = {
 };
 
 const examSchema = {
-  examName: { type: String, required: true, unique: true },
+  examName: { type: String, required: true },
   duration: { type: Number, required: true },
   date: { type: String, required: true },
   startTime: { type: String, required: true },
@@ -40,22 +42,60 @@ const examSchema = {
 };
 
 const classSchema = {
-  className: { type: String, required: true, unique: true },
-  allExams: [examSchema],
+  className: { type: String, required: true },
+  allExams: [String],
 };
 
 const facultySchema = {
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  classes: [classSchema],
+  classes: [String],
 };
 
+////////////////////////// Faculty Schemas END///////////////////////////////////////////////
+
+////////////////////////// Faculty Models START///////////////////////////////////////////////
 var Faculty = mongoose.model("Faculty", facultySchema);
 var FacultyClass = mongoose.model("FacultyClass", classSchema);
 var FacultyExam = mongoose.model("FacultyExam", examSchema);
 
-app.get("/getSignIn", (req, res) => {
+////////////////////////// Faculty Models END///////////////////////////////////////////////
+
+////////////////////////// Faculty Get Post START ///////////////////////////////////////////////
+
+app.post("/facultysignup", (req, res) => {
+  //name,email,password
+  const newFaculty = new Faculty({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newFaculty.save(function (err) {
+    if (!err) {
+      res.send("Successfully added a new Faculty.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/facultyforgotPassword", (req, res) => {
+  //email,newpassword
+  Faculty.findOneAndUpdate(
+    { email: req.body.email },
+    { $set: { password: req.body.newpassword } },
+    { new: true }
+  )
+    .then((faculty) => {
+      res.send({ status: "Successfully Updated Password" });
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+app.get("/facultysignIn", (req, res) => {
+  //email,password
   Faculty.findOne({ email: req.body.email }, { classes: 0, name: 0, __v: 0 })
     .then((faculty) => {
       if (faculty === null) {
@@ -72,69 +112,271 @@ app.get("/getSignIn", (req, res) => {
     .catch((e) => res.status(400).send(e));
 });
 
-app.get("/getAllClass", (req, res) => {
-  Faculty.findById(req.body.fid, {
-    classes: 1,
-    allExams: { $slice: -1 },
-    _id: 0,
-  })
+app.get("/getFacultyAllClass", (req, res) => {
+  //fid
+  Faculty.findById(req.body.fid)
     .then((faculty) => {
-      const allClass = faculty.classes;
+      allClassIds = faculty.classes;
 
-      res.send(allClass);
+      FacultyClass.find(
+        {
+          _id: { $in: allClassIds },
+        },
+        { allExams: 0, __v: 0 }
+      )
+        .then((facultyClass) => {
+          res.send(facultyClass);
+        })
+        .catch((e) => res.status(400).send(e));
     })
     .catch((e) => res.status(400).send(e));
 });
 
-app.post("/signup", (req, res) => {
-  const newFaculty = new Faculty({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
+app.get("/getAllExams", (req, res) => {
+  //cid,
+  FacultyClass.findById(req.body.cid)
+    .then((facultyClass) => {
+      allExamIds = facultyClass.allExams;
+
+      FacultyExam.find(
+        {
+          _id: { $in: allExamIds },
+        },
+        { allQuestions: 0, __v: 0 }
+      )
+        .then((facultyExam) => {
+          res.send(facultyExam);
+        })
+        .catch((e) => res.status(400).send(e));
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+app.get("/getExamQuestions", (req, res) => {
+  //eid
+  FacultyExam.findById(req.body.eid, { __v: 0 })
+    .then((facultyExam) => {
+      res.send(facultyExam);
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+app.post("/editExamQuestions", (req, res) => {
+  //eid,examDetails
+  FacultyExam.findByIdAndUpdate(req.body.eid, req.body.examDetails)
+    .then((facultyExam) => {
+      res.send({ status: "Successfully Updated Exam Details" });
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+app.post("/addClass", (req, res) => {
+  // fid,className,
+  const newClass = new FacultyClass({
+    className: req.body.className,
   });
 
-  newFaculty.save(function (err) {
+  newClass.save(function (err) {
     if (!err) {
-      res.send("Successfully added a new Faculty.");
+      Faculty.findById(req.body.fid)
+        .then((faculty) => {
+          const currentClasses = faculty.classes;
+          faculty.classes = [...currentClasses, newClass._id];
+          return faculty.save();
+        })
+        .then((faculty) => {
+          res.send({ status: "Added class Successfully" });
+        })
+        .catch((e) => res.status(400).send(e));
     } else {
       res.send(err);
     }
   });
 });
 
-app.post("/addClass", (req, res) => {
-  facultyID = req.body.fid;
-  const newClass = new FacultyClass({
-    className: req.body.cname,
+app.post("/addExam", (req, res) => {
+  //examDetails,cid
+  const newExam = new FacultyExam(req.body.examDetails);
+
+  newExam.save(function (err) {
+    if (!err) {
+      FacultyClass.findById(req.body.cid)
+        .then((facultyClass) => {
+          const currentExam = facultyClass.allExams;
+          facultyClass.allExams = [...currentExam, newExam._id];
+          return facultyClass.save();
+        })
+        .then((facultyClass) => {
+          res.send({ status: "Added class Successfully" });
+        })
+        .catch((e) => res.status(400).send(e));
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// app.post("/verifiedResponses", (req, res) => {
+//   //sid,eid,qid,marksAwarded
+//   StudentExamModel.findOne({ studentId: req.body.sid, examId: req.body.eid }, {studentId:0,examId:0,isExamCompleted:0})
+//     .then((studentExamModel) => {
+//       // res.send({ status: "Successfully Updated Password" });
+//     })
+//     .catch((e) => res.status(400).send(e));
+// });
+////////////////////////// Faculty Get Post END ///////////////////////////////////////////////
+
+////////////////////////// Student Schemas START ///////////////////////////////////////////////
+const studentSchema = {
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  classes: [String],
+};
+
+const studentAnswerSchema = new mongoose.Schema({
+  _id: false,
+  questionId: { type: String, required: true, unique: true },
+  answered: String,
+  marksAwarded: Number,
+});
+studentAnswerSchema.index({ questionId: 1 });
+
+const studentExamSchema = new mongoose.Schema({
+  _id: false,
+  studentId: { type: String, required: true },
+  examId: { type: String, required: true },
+  isExamCompleted: { type: String, required: true },
+  studentAnswerSheet: [studentAnswerSchema],
+});
+
+studentExamSchema.index({ studentId: 1, examId: 1 }, { unique: true });
+
+////////////////////////// Student Schemas END  ///////////////////////////////////////////////
+
+////////////////////////// Student Models START ///////////////////////////////////////////////
+const Student = mongoose.model("Student", studentSchema);
+const StudentAnswerModel = mongoose.model(
+  "StudentAnswerModel",
+  studentAnswerSchema
+);
+const StudentExamModel = mongoose.model("StudentExamModel", studentExamSchema);
+////////////////////////// Student  Models END  ///////////////////////////////////////////////
+
+////////////////////////// Student Get Post START ///////////////////////////////////////////////
+
+app.post("/studentSignUp", (req, res) => {
+  //name,email,password
+  const newStudent = new Student({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
   });
 
-  Faculty.findById(req.body.fid)
-    .then((faculty) => {
-      const currentClasses = faculty.classes;
-      faculty.classes = [...currentClasses, newClass];
-      return faculty.save();
+  newStudent.save(function (err) {
+    if (!err) {
+      res.send("Successfully added a new Student.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/studentForgotPassword", (req, res) => {
+  //email,newpassword
+  Student.findOneAndUpdate(
+    { email: req.body.email },
+    { $set: { password: req.body.newpassword } },
+    { new: true }
+  )
+    .then((student) => {
+      res.send({ status: "Successfully Updated Password" });
     })
-    .then((faculty) => {
+    .catch((e) => res.status(400).send(e));
+});
+
+app.get("/studentSignIn", (req, res) => {
+  //email,password
+  Student.findOne({ email: req.body.email }, { classes: 0, name: 0, __v: 0 })
+    .then((student) => {
+      if (student === null) {
+        res.send({ status: "Unregistered User" });
+      } else {
+        if (student.password === req.body.password) {
+          res.send({ status: "Password Verified" });
+        } else {
+          res.send({ status: "Invalid Password" });
+        }
+      }
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+app.post("/addClass", (req, res) => {
+  //sid,cid
+
+  Student.findById(req.body.sid)
+    .then((student) => {
+      const allClasses = student.classes;
+      student.classes = [...allClasses, req.body.cid];
+      return student.save();
+    })
+    .then((student) => {
       res.send({ status: "Added class Successfully" });
     })
     .catch((e) => res.status(400).send(e));
 });
 
-app.post("/addExam", (req, res) => {
-  const newExam = new FacultyExam(req.body.examDetails);
+app.get("/getStudentAllClass", (req, res) => {
+  //sid
+  Student.findById(req.body.sid)
+    .then((student) => {
+      allClassIds = student.classes;
 
-  Faculty.findById(req.body.fid)
-    .then((faculty) => {
-      const currentClass = faculty.classes.id(req.body.cid);
-      const allExams = currentClass.allExams;
-      currentClass.allExams = [...allExams, newExam];
-      return faculty.save();
-    })
-    .then((faculty) => {
-      res.send({ status: "Added Exam Successfully" });
+      FacultyClass.find(
+        {
+          _id: { $in: allClassIds },
+        },
+        { allExams: 0, __v: 0 }
+      )
+        .then((facultyClass) => {
+          res.send(facultyClass);
+        })
+        .catch((e) => res.status(400).send(e));
     })
     .catch((e) => res.status(400).send(e));
 });
+
+app.post("/submitExam", (req, res) => {
+  //studentId,examId,studentAnswerSheet
+  const newStudentExamModel = new StudentExamModel({
+    studentId: req.body.studentId,
+    examId: req.body.examId,
+    studentAnswerSheet: req.body.studentAnswerSheet,
+    isExamCompleted: true,
+  });
+  newStudentExamModel.save(function (err) {
+    if (!err) {
+      res.send("Successfully submitted answers.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// {
+//   "studentId":"60afbc3ef17b5d0fdc93be9e",
+//   "examId":"60afbc3ef17b5d0fdc93be9e",
+//   "studentAnswerSheet":[
+//       {
+//           "questionId":"60afc50124059e354c5be6f5",
+//           "answered":"Hello"
+//       }
+//   ]
+
+// }
+
+////////////////////////// Student Get Post END   ///////////////////////////////////////////////
 
 app.listen(3000, function () {
   console.log(`Server started on port ${port}`);
@@ -143,23 +385,23 @@ app.listen(3000, function () {
 // {
 //   "fid":"60ae7480ac19e6240ce6c9fa",
 //   "cid":"60ae74ec2c71d342c030d4e9",
-//   "examDetails":{
-//       "examName":"IPWT Quiz 1",
-//       "duration":"3",
-//       "date": "24-04-2022",
-//       "startTime": "04:00:00",
-//       "endTime": "05:00:00",
-//       "isCompleted": false,
-//       "allQuestions": [{
-//           "question":"sdfsdksjfkldsjds",
-//           "isText":false,
-//           "options":[{
-//               "value":"Hello"
-//           },
-//           {
-//               "value":"Hi"
-//           }]
-//       }]
+// "examDetails":{
+//     "examName":"IPWT Quiz 1",
+//     "duration":"3",
+//     "date": "24-04-2022",
+//     "startTime": "04:00:00",
+//     "endTime": "05:00:00",
+//     "isCompleted": false,
+//     "allQuestions": [{
+//         "question":"sdfsdksjfkldsjds",
+//         "isText":false,
+//         "options":[{
+//             "value":"Hello"
+//         },
+//         {
+//             "value":"Hi"
+//         }]
+//     }]
 
 //   }
 // }
@@ -177,6 +419,5 @@ app.listen(3000, function () {
 
 // fid 60ae826efc829f0fecf9969a
 // cid 60ae82b389940718f4a4411f
-
 
 //TODO Fetch responses and student id from student database to the faculty for checking
